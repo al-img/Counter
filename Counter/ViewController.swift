54a9d992b6447ccf86e9101f56ba776dea054373
al_img <alimiumk@gmail.com>
2024-03-30 17:33:58 +0300

    Сделана базовая задача

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var tapButton: UIButton!
+    var counterNumbers : Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        counterLabel.text = "0"
+
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tapButtonAction(_ sender: Any) {
+        self.counterNumbers += 1
+        self.counterLabel.text = String(self.counterNumbers)
 
+    }
+    
 }
 
